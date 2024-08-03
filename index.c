e2363a88b5f954c3101425e5ca499721f1158ba9
Ritesh dawale <riteshdawale1596@gmail.com>
2024-08-03 22:43:37 +0530

    added new c program name as index.c

---
+++ index.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main(){
+    int a, b;
+    printf("Enter first Numbers:");
+    scanf("%d", &a);
+
+    printf("Enter second Numbers:");
+    scanf("%d", &b);
+
+    printf("The Addition of two number is %d", a + b);
+    
+   return 0;
+}
